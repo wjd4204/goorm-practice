c33699234410a9f5d51ae6a4841eb00b882751b2
wjd4204 <brian876373@gmail.com>
2024-09-10 10:06:34 +0900

    init

---
+++ reatapi_ex/src/main/java/com/example/reatapi_ex/ReatapiExApplication.java
@@ -0,0 +1,13 @@
+package com.example.reatapi_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReatapiExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReatapiExApplication.class, args);
+    }
+
+}
